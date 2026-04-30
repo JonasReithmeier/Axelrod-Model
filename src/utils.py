de141404a85c43bbcc1846bc307b4b1a97d4f691
jonas <jonasgithub@outlook.de>
2026-04-30 11:02:52 +0200

    feat: utils: alternative colour coding concept

--- src/utils.py
+++ src/utils.py
@@ -10,9 +10,23 @@ def get_different_traits(agent_a, agent_b):
     indices = np.where(agent_a.culture != agent_b.culture)[0]
     return indices
 
+#very primitive colour coding with hasher; Problem: similar pixels completely different colour encoded (hashing)
 def culture_to_combined_int(culture):
     """
     Helper for visualization. Converts the culture array into a single 
     unique number so we can plot it as a color.
     """
     return hash(tuple(culture)) % 1000  # Simplified for heatmap
+
+
+
+''' L2 Norm Länge als Farbwerte; Problem: [3,4] und [4,3] haben gleiche Farbe
+def culture_to_combined_int(culture):
+    """
+    Erzeugt einen Farbwert, bei dem jedes Merkmal gleich stark eingeht.
+    Berechnet die Euklidische Norm (Länge des Vektors).
+    """
+    # Wir behandeln die Merkmale als Koordinaten in einem F-dimensionalen Raum
+    # Jedes Merkmal trägt quadratisch zum Gesamtwert bei.
+    return np.sqrt(np.sum(np.square(culture)))
+'''
